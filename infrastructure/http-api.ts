d76b912cc8c8f5c2057f4bd7f2b8ef414a8580c3
Amir Latypov <Amir.Latypov@gmail.com>
2023-06-08 12:05:49 +0300

    Add auth0 authorizer

--- infrastructure/http-api.ts
+++ infrastructure/http-api.ts
@@ -3,16 +3,34 @@ import * as pulumi from '@pulumi/pulumi';
 import taskRoutes from '@app/resources/task';
 import { lambdaHandler, IResourceRoute } from '@app/helpers';
 
+const config = new pulumi.Config();
+const audience = config.require('audience');
+const issuer = config.require('issuer');
+
 const stack = pulumi.getStack();
 
 const apigw = new aws.apigatewayv2.Api(`${stack}httpApiGateway`, {
   protocolType: 'HTTP',
 });
 
+const authorizer = new aws.apigatewayv2.Authorizer(
+  'authorizer',
+  {
+    apiId: apigw.id,
+    authorizerType: 'JWT',
+    identitySources: ['$request.header.Authorization'],
+    jwtConfiguration: {
+      audiences: [audience],
+      issuer: issuer,
+    },
+  },
+  { dependsOn: apigw },
+);
+
 defineLambdaRole();
 
 // create routes based on the routes defined in the resources folder
-const routes = createRoutes([...taskRoutes]);
+const routes = createRoutes([...taskRoutes], authorizer.id);
 
 const stage = new aws.apigatewayv2.Stage(
   'apiStage',
@@ -54,7 +72,7 @@ function defineLambdaRole() {
   });
 }
 
-function createRoutes(defineRoutes: IResourceRoute[]) {
+function createRoutes(defineRoutes: IResourceRoute[], authorizerId?: pulumi.Output<string>) {
   const routes = [];
 
   for (const route of defineRoutes) {
@@ -79,12 +97,18 @@ function createRoutes(defineRoutes: IResourceRoute[]) {
       },
     );
 
-    routes.push(createRouteForLambda(apigw, lambda, route.path, name));
+    routes.push(createRouteForLambda(apigw, lambda, route.path, name, authorizerId));
   }
   return routes;
 }
 
-function createRouteForLambda(api: aws.apigatewayv2.Api, lambda: aws.lambda.Function, routeKey: string, name: string) {
+function createRouteForLambda(
+  api: aws.apigatewayv2.Api,
+  lambda: aws.lambda.Function,
+  routeKey: string,
+  name: string,
+  authorizerId?: pulumi.Output<string>,
+) {
   const integration = new aws.apigatewayv2.Integration(`${name}-integration`, {
     apiId: api.id,
     integrationType: 'AWS_PROXY',
@@ -98,6 +122,8 @@ function createRouteForLambda(api: aws.apigatewayv2.Api, lambda: aws.lambda.Func
   return new aws.apigatewayv2.Route(`${name}-route`, {
     apiId: api.id,
     routeKey,
+    authorizationType: 'JWT',
+    authorizerId,
     target: pulumi.interpolate`integrations/${integration.id}`,
   });
 }
